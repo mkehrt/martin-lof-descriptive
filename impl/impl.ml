b34462273fddacefb842cfd788daf58b80a6a9de
Matthew Kehrt <mkehrt@roseandsigil.net>
2016-07-26 23:40:35 -0700

    More.

--- impl/impl.ml
+++ impl/impl.ml
@@ -8,8 +8,19 @@ module SecondOrderExistentialPrenex = struct
 
   type kind = KindTy | KindArrow of kind * kind
 
-  type prenex = SecondOrderExistentialPrenex
-    of Variables.ty var * kind * prenex
-  | FirstOrder
+  type secondOrderExistentialType =
+    SecondOrderExistential of Variables.ty var * kind * secondOrderExistentialType
+  | FirstOrder of firstOrder
+
+  type secondOrderPath = Head of 
+
+  type firstOrderType
+    Path2 of secondOrderPath
+  | Var1 of Variables.exp Variables
+  | FirstOrderExistential of Variables.exp * firstOrderType * firstOrderType
+  | FirstOrderUniversal of Variables.exp * firstOrderType * firstOrderType
+  | And of firstOrderType * firstOrderType
+  | Or of firstOrderType * firstOrderType
+  | Implies of firstOrderType * firstOrderType
 
 end
